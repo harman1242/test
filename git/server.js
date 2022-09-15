c0c54015c59995e2a6aed4d2cd0c47f70c2ac1dc
harman1242 <you@example.com>
2022-09-15 12:04:34 +0530

    server harryl

--- git/server.js
+++ git/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+var http = require('http');
+http.createServer(function(req, res){
+res.writeHead(200, { 'Content-Type': 'application/json'
+})
+res.end("hello world!");
+}).listen(3000,"127.0.0.1")
+// app.use(express.json());
 
-app.use(express.json());
 
-
-app.listen(3001,(()=>{
-    console.log("listen on 3001")
-}))
+// app.listen(3001,(()=>{
+//     console.log("listen on 3001")
+// }))
